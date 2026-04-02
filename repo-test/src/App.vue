a3da0fbbb0af44b495abcf7d02d12aa19250a6af
hyunseo2503 <hyunseo2503@gmail.com>
2026-04-02 17:27:22 +0900

    'feat:22'

--- repo-test/src/App.vue
+++ repo-test/src/App.vue
@@ -17,8 +17,8 @@ import HelloWorld from './components/HelloWorld.vue';
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About111222</RouterLink>
+        <RouterLink to="/">Home집에가쟈</RouterLink>
+        <RouterLink to="/about">About111222333333</RouterLink>
       </nav>
     </div>
   </header>
