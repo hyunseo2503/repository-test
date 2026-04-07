185bbeb4f6708597be32a1ba8ec92cd0d61a63f3
jeonggil <jeonggilahn@gmail.com>
2026-04-07 17:47:05 +0900

    feat : tst

--- repo-test/src/App.vue
+++ repo-test/src/App.vue
@@ -18,7 +18,7 @@ import HelloWorld from './components/HelloWorld.vue';
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About111222</RouterLink>
+        <RouterLink to="/about">About111</RouterLink>
       </nav>
     </div>
   </header>
